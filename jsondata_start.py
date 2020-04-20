674a9ee423fd57450c5ce03aa22d120e0b11a9ba
Prabin Parajuli <prabin.b9@gmail.com>
2020-04-20 14:07:34 +0200

    Update jsondata_start.py

--- jsondata_start.py
+++ jsondata_start.py
@@ -10,16 +10,23 @@ def printResults(data):
   theJSON = json.loads(data)
   
   # now we can access the contents of the JSON like any other Python object
-
+  if "title" in theJSON["metadata"]:
+    print(theJSON["metadata"]["title"])
   
   # output the number of events, plus the magnitude and each event name  
-
+  count = theJSON["metadata"]["count"]
+  print(str(count) + " events recorded")
 
   # for each event, print the place where it occurred
-
+  for i in theJSON["features"]:
+    print(i["properties"]["place"])
+  print("----------\n")
 
   # print the events that only have a magnitude greater than 4
-
+  for i in theJSON["features"]:
+    if i["properties"]["mag"] >= 4.0:
+      print ("%2.1f" % i["properties"]["mag"], i["properties"]["place"])
+    print(".......\n")
       
   # print only the events where at least 1 person reported feeling something
 
